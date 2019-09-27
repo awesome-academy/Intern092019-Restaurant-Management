af5e19b7672780a48f16726520196eb0c59140eb
Nguyen Huy <nguyen.van.huy@sun-asterisk.com>
2019-09-27 11:07:08 +0700

    init project

---
+++ config/application.rb
@@ -0,0 +1,13 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module RailsTutorial
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = %w(en vi)
+  end
+end
